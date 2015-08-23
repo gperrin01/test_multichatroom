8d89a0bce9d135e5b8fbc1efacef0456390fc999
Guillaume Perrin <gperrin01@gmail.com>
2015-08-23 18:41:36 +0100

    basic socket structure

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('/views', './views');
 app.set('view engine', 'ejs');
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server); // see below for all the socket events and messages
 
 server.listen(8080, function(){
   console.log('listening on port', 8080);
@@ -26,3 +26,30 @@ server.listen(8080, function(){
 app.get('/', function(req, res){
   res.render('index')
 })
+
+
+
+/*************************************
+// SOCKET
+*************************************/
+
+io.sockets.on('connection', function(socket) {
+
+  // Let's assume a default connection to the "General" room
+  socket.room = 'general';
+  socket.join(socket.room);
+  socket.emit('connected');
+  socket.emit('newRoom', socket.room)
+
+  // chat message broddcasted to all members of the current room
+  socket.on('chat', function(data) {
+    socket.broadcast.to(socket.room).emit('chat', data);
+  });
+
+  // leave a room and enter a new one
+  socket.on('switchRoom', function(newRoom){
+    socket.leave(socket.room);
+    socket.room = newRoom;
+    socket.join(socket.room);
+    socket.emit('newRoom', socket.room)
+  })
